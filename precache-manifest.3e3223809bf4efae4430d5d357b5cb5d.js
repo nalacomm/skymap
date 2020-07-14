30c502e27b230e1058b4e8df082e6f4eec3c9c5f
Jamtech <jamtechba@gmail.com>
2020-07-14 16:04:52 +0530

    Updates

---
+++ precache-manifest.3e3223809bf4efae4430d5d357b5cb5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6131f17f1596e4d8752f12d502fc958",
+    "url": "/skymap/index.html"
+  },
+  {
+    "revision": "e7eaf20c7c2116dcb03d",
+    "url": "/skymap/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e918a03ed69ee36cc2fc",
+    "url": "/skymap/static/css/main.10c767c6.chunk.css"
+  },
+  {
+    "revision": "e7eaf20c7c2116dcb03d",
+    "url": "/skymap/static/js/2.b9c5c657.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/skymap/static/js/2.b9c5c657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e918a03ed69ee36cc2fc",
+    "url": "/skymap/static/js/main.5e89b196.chunk.js"
+  },
+  {
+    "revision": "6ff5ef34ad7d492c96b8",
+    "url": "/skymap/static/js/runtime-main.4cc3ab59.js"
+  },
+  {
+    "revision": "87c696b907acfe28a6b4c99aae3baba7",
+    "url": "/skymap/static/media/rectangle.87c696b9.svg"
+  },
+  {
+    "revision": "19b34dbfefd110a8b0ed93bb0bbc6c7d",
+    "url": "/skymap/static/media/star.19b34dbf.svg"
+  },
+  {
+    "revision": "d7fed549a739c130145cfb87226a7e4b",
+    "url": "/skymap/static/media/triangle.d7fed549.svg"
+  }
+]);
